0ee287b69313af5373edda1f6a2969ac48afc690
Casey <14831419+CaseyFaist@users.noreply.github.com>
2022-03-27 18:07:17 -0400

    First commit

---
+++ estimate.py
@@ -0,0 +1,3 @@
+from foods import *
+
+print(foods.raspberries)
---
+++ foods.py
@@ -0,0 +1,95 @@
+cherries = {
+	weight: 140,
+	calories: 88,
+	fat: 0,
+	saturated: 0,
+	sodium: 0,
+	carb: 16,
+	fiber: 3,
+	sugar: 18,
+	added_sugar: 0,
+	protein: 1
+}
+
+blueberries = {
+	weight: 230,
+	calories: 117,
+	fat: 1.5,
+	saturated: 0,
+	sodium: 15,
+	carb: 28,
+	fiber: 6,
+	sugar: 20,
+	added_sugar: 0,
+	protein: 1
+}
+
+bananas = {
+	weight: 118,
+	calories: 105,
+	fat: 0,
+	saturated: 0,
+	sodium: 15,
+	carb: 27,
+	fiber: 3,
+	sugar: 15,
+	added_sugar: 0,
+	protein: 1
+}
+
+raspberries = {
+	weight: 250,
+	calories: 70,
+	fat: 1,
+	saturated: 0,
+	sodium: 15,
+	carb: 15,
+	fiber: 5,
+	sugar: 8,
+	added_sugar: 0,
+	protein: 1
+}
+
+acai_puree = {
+	weight: 230,
+	calories: 140,
+	fat: 11,
+	saturated: 0,
+	sodium: 15,
+	carb: 15,
+	fiber: 12,
+	sugar: 0,
+	added_sugar: 0,
+	protein: 2
+}
+
+kale = {
+	weight: 0,
+	calories: 0
+}
+
+psyllium_husk = {
+	weight: 5,
+	calories: 18,
+	fat: 0,
+	saturated: 0,
+	sodium: 0,
+	carb: 4,
+	fiber: 4,
+	sugar: 0,
+	added_sugar: 0,
+	protein: 0
+}
+
+acai_and_cherry_smoothie = {
+	weight: 196,
+	calories: 140,
+	fat: 2,
+	saturated: 0,
+	sodium: 15,
+	carb: 30,
+	fiber: 6,
+	sugar: 18,
+	added_sugar: 0,
+	protein: 3
+}
